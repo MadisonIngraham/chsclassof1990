b36548931a46ec6df2b2def1db957bbe47b7b155
MadisonIngraham <madi.ingraham@gmail.com>
2020-03-01 13:13:03 -0700

    added and

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -10,7 +10,7 @@
         </div>
         <div class="d-flex flex-column align-items-center">
           <h3>Coming Soon!</h3>
-          <p>Friday, July 31st & Saturday, August 1st</p>
+          <p>Friday, July 31st and Saturday, August 1st</p>
           <p>Stay tuned for 2020 reunion details.</p>
         </div>
       </div>
